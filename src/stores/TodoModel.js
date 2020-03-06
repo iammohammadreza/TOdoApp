2df408dbef53f0680b682b1b1bce7d07104afcd5
iammohammadreza <mrh6477@gmail.com>
2020-03-06 12:45:23 +0330

    first commit

---
+++ src/stores/TodoModel.js
@@ -0,0 +1,23 @@
+import { observable, action } from "mobx";
+
+export default class TodoMoodel {
+  store;
+  // @observable
+  id;
+  @observable title;
+  @observable completed;
+  @observable destroy;
+  constructor(store, title, completed, id, destroy) {
+    this.title = title;
+    this.completed = completed;
+    this.id = id;
+    this.store = store;
+    this.destroy = destroy;
+  }
+  @action
+  toggle() {
+    this.completed = !this.completed;
+  }
+
+ 
+}
